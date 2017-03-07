8c5706178209b8e0294f87fe2cbbeaee236c8334
andreaGhisa <aghisa@sdl.com>
2017-03-07 11:53:38 +0200

    Project Template  documentation.

--- Sdl.Community.GroupShareKit/Clients/IProjectsTemplateClient.cs
+++ Sdl.Community.GroupShareKit/Clients/IProjectsTemplateClient.cs
@@ -4,17 +4,86 @@ using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sdl.Community.GroupShareKit.Exceptions;
 using Sdl.Community.GroupShareKit.Models.Response;
 
 namespace Sdl.Community.GroupShareKit.Clients
 {
     public interface IProjectsTemplateClient
     {
-
+        /// <summary>
+        ///Gets all templates
+        /// </summary>
+        /// <remarks>
+        /// This method requires authentication.
+        /// See the <a href="http://sdldevelopmentpartners.sdlproducts.com/documentation/api">API documentation</a> for more information.
+        /// </remarks>
+        /// <exception cref="AuthorizationException">
+        /// Thrown when the current user does not have permission to make the request.
+        /// </exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>A List of <see cref="ProjectTemplates"/></returns>
         Task<IReadOnlyList<ProjectTemplates>> GetAllTemplates();
+
+        /// <summary>
+        ///Creates a template
+        /// </summary>
+        /// <param name="projectRequest"><see cref="ProjectTemplates"/></param>
+        /// <remarks>
+        /// This method requires authentication.
+        /// See the <a href="http://sdldevelopmentpartners.sdlproducts.com/documentation/api">API documentation</a> for more information.
+        /// </remarks>
+        /// <exception cref="AuthorizationException">
+        /// Thrown when the current user does not have permission to make the request.
+        /// </exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>Id of created template/></returns>
         Task<string> CreateTemplate(ProjectTemplates projectRequest);
+
+        /// <summary>
+        ///Get a template by id
+        /// </summary>
+        /// <param name="templateId">string</param>
+        /// <remarks>
+        /// This method requires authentication.
+        /// See the <a href="http://sdldevelopmentpartners.sdlproducts.com/documentation/api">API documentation</a> for more information.
+        /// </remarks>
+        /// <exception cref="AuthorizationException">
+        /// Thrown when the current user does not have permission to make the request.
+        /// </exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>Te contend of template in a string/></returns>
         Task<string> GetTemplateById(string templateId);
+
+        /// <summary>
+        ///Deletes a template 
+        /// </summary>
+        /// <param name="id">string</param>
+        /// <remarks>
+        /// This method requires authentication.
+        /// See the <a href="http://sdldevelopmentpartners.sdlproducts.com/documentation/api">API documentation</a> for more information.
+        /// </remarks>
+        /// <exception cref="AuthorizationException">
+        /// Thrown when the current user does not have permission to make the request.
+        /// </exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
         Task Delete(string id);
+
+        /// <summary>
+        ///Uploades a template to a newly created project 
+        /// This method shoulg be called after you create a project in order to add the template
+        /// </summary>
+        /// <param name="templateId">string</param>
+        /// <param name="projectTemplate">byte[]</param>
+        /// <param name="templateName">string</param>
+        /// <remarks>
+        /// This method requires authentication.
+        /// See the <a href="http://sdldevelopmentpartners.sdlproducts.com/documentation/api">API documentation</a> for more information.
+        /// </remarks>
+        /// <exception cref="AuthorizationException">
+        /// Thrown when the current user does not have permission to make the request.
+        /// </exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
         Task<string> UploadProjectTemplate(string templateId,byte[] projectTemplate,string templateName);
     }
 }
--- Sdl.Community.GroupShareKit/Clients/ProjectTemplatesClient.cs
+++ Sdl.Community.GroupShareKit/Clients/ProjectTemplatesClient.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Sdl.Community.GroupShareKit.Exceptions;
 using Sdl.Community.GroupShareKit.Helpers;
 using Sdl.Community.GroupShareKit.Http;
 using Sdl.Community.GroupShareKit.Models.Response;
@@ -16,11 +17,36 @@ namespace Sdl.Community.GroupShareKit.Clients
         {
         }
 
+        /// <summary>
+        ///Gets all templates
+        /// </summary>
+        /// <remarks>
+        /// This method requires authentication.
+        /// See the <a href="http://sdldevelopmentpartners.sdlproducts.com/documentation/api">API documentation</a> for more information.
+        /// </remarks>
+        /// <exception cref="AuthorizationException">
+        /// Thrown when the current user does not have permission to make the request.
+        /// </exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>A List of <see cref="ProjectTemplates"/></returns>
         public async Task<IReadOnlyList<ProjectTemplates>> GetAllTemplates()
         {
            return await ApiConnection.GetAll<ProjectTemplates>(ApiUrls.ProjectTemplates(),null);
         }
 
+        /// <summary>
+        ///Creates a template
+        /// </summary>
+        /// <param name="templateRequest"><see cref="ProjectTemplates"/></param>
+        /// <remarks>
+        /// This method requires authentication.
+        /// See the <a href="http://sdldevelopmentpartners.sdlproducts.com/documentation/api">API documentation</a> for more information.
+        /// </remarks>
+        /// <exception cref="AuthorizationException">
+        /// Thrown when the current user does not have permission to make the request.
+        /// </exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>Id of created template/></returns>
         public async Task<string> CreateTemplate(ProjectTemplates templateRequest)
         {
             var templateId=  await ApiConnection.Post<string>(ApiUrls.ProjectTemplates(), templateRequest, "application/json");
@@ -28,18 +54,57 @@ namespace Sdl.Community.GroupShareKit.Clients
             return templateId;
         }
 
+        /// <summary>
+        ///Get a template by id
+        /// </summary>
+        /// <param name="templateId">string</param>
+        /// <remarks>
+        /// This method requires authentication.
+        /// See the <a href="http://sdldevelopmentpartners.sdlproducts.com/documentation/api">API documentation</a> for more information.
+        /// </remarks>
+        /// <exception cref="AuthorizationException">
+        /// Thrown when the current user does not have permission to make the request.
+        /// </exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>Te contend of template in a string/></returns>
         public async Task<string> GetTemplateById(string templateId)
         {
             Ensure.ArgumentNotNullOrEmptyString(templateId, "templateId");
             return await ApiConnection.Get<string>(ApiUrls.ProjectTemplates(templateId), null);
         }
 
+        /// <summary>
+        ///Deletes a template 
+        /// </summary>
+        /// <param name="templateId">string</param>
+        /// <remarks>
+        /// This method requires authentication.
+        /// See the <a href="http://sdldevelopmentpartners.sdlproducts.com/documentation/api">API documentation</a> for more information.
+        /// </remarks>
+        /// <exception cref="AuthorizationException">
+        /// Thrown when the current user does not have permission to make the request.
+        /// </exception>
         public async Task Delete(string templateId)
         {
             Ensure.ArgumentNotNullOrEmptyString(templateId, "templateId");
             await ApiConnection.Delete(ApiUrls.ProjectTemplates(templateId));
         }
 
+        /// <summary>
+        ///Uploades a template to a newly created project 
+        /// This method shoulg be called after you create a project in order to add the template
+        /// </summary>
+        /// <param name="templateId">string</param>
+        /// <param name="projectTemplate">byte[]</param>
+        /// <param name="templateName">string</param>
+        /// <remarks>
+        /// This method requires authentication.
+        /// See the <a href="http://sdldevelopmentpartners.sdlproducts.com/documentation/api">API documentation</a> for more information.
+        /// </remarks>
+        /// <exception cref="AuthorizationException">
+        /// Thrown when the current user does not have permission to make the request.
+        /// </exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
         public async Task<string> UploadProjectTemplate(string templateId, byte[] projectTemplate,string templateName)
         {
             Ensure.ArgumentNotNullOrEmptyString(templateId, "templateId");
