3fda0f286366948a877990dd086d6dfd19c972f0
Andrew Jameson <andrew_jameson@hotmail.com>
2018-05-17 15:14:31 +0100

    Modified TwelveHourDateTimeConverter.cs to query env vars for a custom date format for MultiTerm termbases. If not present in env vars then a default (using US M/dd/yyyy format) is used.
    Also updated to .NETStandard 2.0.

--- Sdl.Community.GroupShareKit/TwelveHourDateTimeConverter.cs
+++ Sdl.Community.GroupShareKit/TwelveHourDateTimeConverter.cs
@@ -3,10 +3,14 @@ using Newtonsoft.Json.Converters;
 using System;
 using System.Globalization;
 
+
 namespace Sdl.Community.GroupShareKit
 {
     public class TwelveHourDateTimeConverter : IsoDateTimeConverter
     {
+        private static string _dateFormatStr;
+
+
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             if (objectType != typeof(DateTime?))
@@ -14,16 +18,35 @@ namespace Sdl.Community.GroupShareKit
                 throw new Exception($"The {nameof(TwelveHourDateTimeConverter)} class expects to convert to DateTime, not {objectType.Name}.");
             }
 
+            EnsureDateFormatString();
+
             var incomingVal = reader.Value.ToString();
 
             if (new System.Text.RegularExpressions.Regex(@"(\d{1,2}\/\d{1,2}\/\d{4} \d{1,2}:\d{1,2} (AM)|(PM))").IsMatch(incomingVal))
             {
                 return DateTime.ParseExact(incomingVal,
-                                           "dd/M/yyyy h:mm tt",
+                                           _dateFormatStr,
                                            CultureInfo.InvariantCulture);
             }
 
             return base.ReadJson(reader, objectType, existingValue, serializer);
+        }
+
+
+        private static void EnsureDateFormatString()
+        {
+            if (!String.IsNullOrWhiteSpace(_dateFormatStr))
+            {
+                return;
+            }
+
+            if (!String.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("multiterm.dateFormatStr", EnvironmentVariableTarget.User)))
+            {
+                _dateFormatStr = Environment.GetEnvironmentVariable("multiterm.dateFormatStr", EnvironmentVariableTarget.User);
+                return;
+            }
+
+            _dateFormatStr = "M/dd/yyyy h:mm tt";
         }
     }
 }
