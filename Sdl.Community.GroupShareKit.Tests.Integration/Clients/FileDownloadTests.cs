182a8183c2324e57ac3ec03dc870ecda91147f23
Andrei Elvedi <aelvedi@sdl.com>
2019-07-10 14:54:01 +0300

    [LTGS-4470] Commented out tests

--- Sdl.Community.GroupShareKit.Tests.Integration/Clients/FileDownloadTests.cs
+++ Sdl.Community.GroupShareKit.Tests.Integration/Clients/FileDownloadTests.cs
@@ -22,27 +22,27 @@ namespace Sdl.Community.GroupShareKit.Tests.Integration.Clients
             Assert.True(request != null);
         }
 
-        [Fact]
-        public async Task DownloadNative()
-        {
-            var groupShareClient = await Helper.GetGroupShareClient();
+        //[Fact]
+        //public async Task DownloadNative()
+        //{
+        //    var groupShareClient = await Helper.GetGroupShareClient();
 
-            var file = await groupShareClient.Project.DownloadNative("c1f47d9c-a9dd-4069-b636-3405d4fb98a8");
+        //    var file = await groupShareClient.Project.DownloadNative("c1f47d9c-a9dd-4069-b636-3405d4fb98a8");
 
-            Assert.True(file != null);
-        }
+        //    Assert.True(file != null);
+        //}
 
-        [Theory]
-        [InlineData("c1f47d9c-a9dd-4069-b636-3405d4fb98a8")]
-        public async Task Finalize(string projectId)
-        {
-            var groupShareClient = await Helper.GetGroupShareClient();
-            var languageFilesId = new List<string> { "23ddbfcf-a015-47ff-9e05-f62a3bfb783a" };
+        //[Theory]
+        //[InlineData("c1f47d9c-a9dd-4069-b636-3405d4fb98a8")]
+        //public async Task Finalize(string projectId)
+        //{
+        //    var groupShareClient = await Helper.GetGroupShareClient();
+        //    var languageFilesId = new List<string> { "23ddbfcf-a015-47ff-9e05-f62a3bfb783a" };
 
-            var files = await groupShareClient.Project.Finalize(projectId, languageFilesId);
+        //    var files = await groupShareClient.Project.Finalize(projectId, languageFilesId);
 
-            Assert.True(files != null);
-        }
+        //    Assert.True(files != null);
+        //}
 
         [Theory]
         [InlineData("c1f47d9c-a9dd-4069-b636-3405d4fb98a8")]
