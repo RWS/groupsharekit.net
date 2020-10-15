9bcf141a862b96e22bc7a05ba6fa4bf84ecea40c
GLOBAL\mharangus <mharangus@sdl.com>
2020-10-15 16:51:31 +0300

    Add tests and cover new endpoints for reporting service predefined reports.

---
+++ Sdl.Community.GroupShareKit/Clients/IReportingClient.cs
@@ -0,0 +1,55 @@
+﻿using Sdl.Community.GroupShareKit.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sdl.Community.GroupShareKit.Clients
+{
+    public interface IReportingClient
+    {
+		/// <summary>
+		/// Returns the Predefined Projects report data
+		/// </summary>
+		/// <param name="options"></param>
+		/// <remarks>
+		///  This method requires authentication.
+		///  See the <a href="http://gs2017dev.sdl.com:41234/documentation/api/index#/">API documentation</a> for more information.
+		///  </remarks>
+		///  <exception cref="AuthorizationException">
+		///  Thrown when the current user does not have permission to make the request.
+		///  </exception>
+		///  <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+		Task<List<ReportingServiceProjects>> PredefinedProjects(PredefinedReportsFilters filters);
+
+		/// <summary>
+		/// Returns the Predefined Tasks report data
+		/// </summary>
+		/// <param name="options"></param>
+		/// <remarks>
+		///  This method requires authentication.
+		///  See the <a href="http://gs2017dev.sdl.com:41234/documentation/api/index#/">API documentation</a> for more information.
+		///  </remarks>
+		///  <exception cref="AuthorizationException">
+		///  Thrown when the current user does not have permission to make the request.
+		///  </exception>
+		///  <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+		Task<List<ReportingServiceTasks>> PredefinedTasks(PredefinedReportsFilters filters);
+
+		/// <summary>
+		/// Returns the Predefined TmLeverage report data
+		/// </summary>
+		/// <param name="options"></param>
+		/// <remarks>
+		///  This method requires authentication.
+		///  See the <a href="http://gs2017dev.sdl.com:41234/documentation/api/index#/">API documentation</a> for more information.
+		///  </remarks>
+		///  <exception cref="AuthorizationException">
+		///  Thrown when the current user does not have permission to make the request.
+		///  </exception>
+		///  <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+		Task<List<ReportingServiceTmLeverage>> PredefinedTmLeverage(PredefinedReportsFilters filters);
+
+	}
+}
---
+++ Sdl.Community.GroupShareKit/Clients/ReportingClient.cs
@@ -0,0 +1,79 @@
+﻿using Sdl.Community.GroupShareKit.Helpers;
+using Sdl.Community.GroupShareKit.Http;
+using Sdl.Community.GroupShareKit.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sdl.Community.GroupShareKit.Clients
+{
+    public class ReportingClient : ApiClient, IReportingClient
+    {
+        public ReportingClient(IApiConnection apiConnection) : base(apiConnection)
+        {
+        }
+
+        public Task<List<ReportingServiceProjects>> PredefinedProjects(PredefinedReportsFilters predefinedFilters)
+        {
+            var predefinedProjectsUrl = ApiUrls.GetPredefinedProjectsData();
+
+            var filterParameters = new Dictionary<string, string>()
+            {
+                { "showAll" , predefinedFilters.ShowAll.ToString()},
+                { "publishStart", predefinedFilters.PublishStart.ToString()},
+                { "publishEnd", predefinedFilters.PublishEnd.ToString()},
+                { "dueStart", predefinedFilters.DueStart.ToString()},
+                { "dueEnd", predefinedFilters.DueEnd.ToString()},
+                { "status" , predefinedFilters.Status.ToString() },
+                { "OrganizationPath", predefinedFilters.OrganizationPath},
+                { "SourceLanguages", predefinedFilters.SourceLanguages},
+                { "TargetLanguages", predefinedFilters.TargetLanguages},
+                { "AssignedUserIds", predefinedFilters.AssignedUsersIds}          
+            };
+
+            return ApiConnection.Get<List<ReportingServiceProjects>>(predefinedProjectsUrl, filterParameters);
+        }
+
+        public Task<List<ReportingServiceTasks>> PredefinedTasks(PredefinedReportsFilters predefinedFilters)
+        {
+            var predefinedTasksUrl = ApiUrls.GetPredefinedProjectsData();
+
+            var filterParameters = new Dictionary<string, string>()
+            {
+                { "showAll" , predefinedFilters.ShowAll.ToString()},
+                { "publishStart", predefinedFilters.PublishStart.ToString()},
+                { "publishEnd", predefinedFilters.PublishEnd.ToString()},
+                { "dueStart", predefinedFilters.DueStart.ToString()},
+                { "dueEnd", predefinedFilters.DueEnd.ToString()},
+                { "status" , predefinedFilters.Status.ToString() },
+                { "OrganizationPath", predefinedFilters.OrganizationPath},
+                { "SourceLanguages", predefinedFilters.SourceLanguages},
+                { "TargetLanguages", predefinedFilters.TargetLanguages},
+                { "AssignedUserIds", predefinedFilters.AssignedUsersIds}
+            };
+            return ApiConnection.Get<List<ReportingServiceTasks>>(predefinedTasksUrl, filterParameters);
+        }
+
+        public Task<List<ReportingServiceTmLeverage>> PredefinedTmLeverage(PredefinedReportsFilters predefinedFilters)
+        {
+            var predefinedTmLeverageUrl = ApiUrls.GetPredefinedProjectsData();
+
+            var filterParameters = new Dictionary<string, string>()
+            {
+                { "showAll" , predefinedFilters.ShowAll.ToString()},
+                { "publishStart", predefinedFilters.PublishStart.ToString()},
+                { "publishEnd", predefinedFilters.PublishEnd.ToString()},
+                { "dueStart", predefinedFilters.DueStart.ToString()},
+                { "dueEnd", predefinedFilters.DueEnd.ToString()},
+                { "status" , predefinedFilters.Status.ToString() },
+                { "OrganizationPath", predefinedFilters.OrganizationPath},
+                { "SourceLanguages", predefinedFilters.SourceLanguages},
+                { "TargetLanguages", predefinedFilters.TargetLanguages},
+                { "AssignedUserIds", predefinedFilters.AssignedUsersIds}
+            };
+            return ApiConnection.Get<List<ReportingServiceTmLeverage>>(predefinedTmLeverageUrl, filterParameters);
+        }
+    }
+}
