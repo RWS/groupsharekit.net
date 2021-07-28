9b4213d51c841232cd95e3834c513b5bd55060b6
Mihael-Harangus <72926838+Mihael-Harangus@users.noreply.github.com>
2021-07-28 11:25:32 +0300

    Add tests and endpoints for MT Cloud feature. (#97)
    * Add tests and endpoints for MT Cloud feature.
    
    * Remove hardcoded user and rename test methods.
    
    Co-authored-by: Balázs Zoltán <balazs.zoltan@gmail.com>

---
+++ Sdl.Community.GroupShareKit/Models/Response/MtCloudToken.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sdl.Community.GroupShareKit.Models.Response
+{
+    public class MtCloudToken
+    {
+        public string AccessToken { get; set; }
+
+        public int ValidityInSeconds { get; set; }
+
+        public string TokenType { get; set; }
+
+        public DateTime ExpiresAt { get; set; }
+    }
+}
