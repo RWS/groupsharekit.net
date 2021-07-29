88ecdfeefc6fabfb9d777b966c7935827e3c0249
Mihael-Harangus <72926838+Mihael-Harangus@users.noreply.github.com>
2021-07-29 18:34:45 +0300

    Remove unecessary usings (#109)

--- Sdl.Community.GroupShareKit/Helpers/FilterExpression.cs
+++ Sdl.Community.GroupShareKit/Helpers/FilterExpression.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Sdl.Community.GroupShareKit.Models.Response;
 using Sdl.Community.GroupShareKit.Models.Response.TranslationMemory;
 using Sdl.Core.Bcm.BcmModel;
