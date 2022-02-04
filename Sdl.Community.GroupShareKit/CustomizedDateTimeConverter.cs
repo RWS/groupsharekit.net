b31060b24fe72d1ce818c729b175100f82c50181
Raphael Batel <rbatel@sdl.com>
2022-02-04 19:32:14 +0200

    Fix issue with date formats from MT RestAPI

--- Sdl.Community.GroupShareKit/CustomizedDateTimeConverter.cs
+++ Sdl.Community.GroupShareKit/CustomizedDateTimeConverter.cs
@@ -9,23 +9,18 @@ namespace Sdl.Community.GroupShareKit
     /// <summary>
     /// This class is necessary since the GroupShare (MultiTerm) RestAPI returns termbase creation date format depending on server settings (LTGS-9908).
     /// </summary>
-    /// <remarks>
-    /// - Unlike the name suggests, this converter can parse a DateTime string for any given format string, not only 12 hour formats. <b/>
-    /// - To configure this converter set environment variable multiterm.dateFormatStr according to https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings
-    /// </remarks>
-    public class TwelveHourDateTimeConverter : IsoDateTimeConverter
+    public class CustomizedDateTimeConverter : IsoDateTimeConverter
     {
-        private const string _dateFormatStringEnvironmentVariable = "multiterm.dateFormatStr";
-        private const string _defaultDateFormatString = "M/dd/yyyy h:mm tt";
-
         private static Lazy<string> _dateFormatString = new Lazy<string>(() => GetDateFormatStringSetting());
+
+        private const string _defaultDateFormatString = "M/dd/yyyy h:mm tt";
 
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             if (objectType != typeof(DateTime?))
             {
-                throw new Exception($"The {nameof(TwelveHourDateTimeConverter)} class expects to convert to DateTime, not {objectType.Name}.");
+                throw new Exception($"The {nameof(CustomizedDateTimeConverter)} class expects to convert to DateTime, not {objectType.Name}.");
             }
 
             var incomingVal = reader.Value.ToString();
@@ -42,8 +37,7 @@ namespace Sdl.Community.GroupShareKit
 
         private static string GetDateFormatStringSetting()
         {
-            var settingValue = Environment.GetEnvironmentVariable(_dateFormatStringEnvironmentVariable)
-                ?? Environment.GetEnvironmentVariable(_dateFormatStringEnvironmentVariable, EnvironmentVariableTarget.User);
+            var settingValue = Environment.GetEnvironmentVariable("multiterm.dateFormatStr", EnvironmentVariableTarget.User);
             return string.IsNullOrWhiteSpace(settingValue) ? null : settingValue;
         }
     }
