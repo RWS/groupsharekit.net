2701f19ad3c7bf4f4a5b6e4e780892807c0f70ac
Noris Gavrilas <ngavrilas@sdl.com>
2023-08-01 16:45:16 +0300

    Added description to enum values

--- Sdl.Community.GroupShareKit/Clients/ExportPredefinedReportsFilters.cs
+++ Sdl.Community.GroupShareKit/Clients/ExportPredefinedReportsFilters.cs
@@ -1,4 +1,6 @@
-﻿namespace Sdl.Community.GroupShareKit.Clients
+﻿using System.ComponentModel;
+
+namespace Sdl.Community.GroupShareKit.Clients
 {
     public class ExportPredefinedReportsFilters : PredefinedReportsFilters
     {
@@ -8,14 +10,23 @@
 
     public enum ReportLanguage
     {
+        [Description("English")]
         En,
+        [Description("German")]
         De,
+        [Description("Spanish")]
         Es,
+        [Description("French")]
         Fr,
+        [Description("Japanese")]
         Ja,
+        [Description("Italian")]
         It,
+        [Description("Korean")]
         Ko,
+        [Description("Russian")]
         Ru,
+        [Description("Chinese")]
         Zh
     }
 }
