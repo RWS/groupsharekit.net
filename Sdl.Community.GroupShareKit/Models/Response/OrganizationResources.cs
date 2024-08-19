b83791b0666d7f0f11f016fa76362b35a6de6d5d
Alin Soritau <asoritau@sdl.com>
2024-08-19 17:34:55 +0300

    Added spacing betwwen properties in OrganizationResources.cs

--- Sdl.Community.GroupShareKit/Models/Response/OrganizationResources.cs
+++ Sdl.Community.GroupShareKit/Models/Response/OrganizationResources.cs
@@ -9,22 +9,27 @@ namespace Sdl.Community.GroupShareKit.Models.Response
         /// Gets or sets the organization resource id
         /// </summary>
         public Guid Id { get; set; }
+        
         /// <summary>
         /// Gets or sets the name
         /// </summary>
         public string Name { get; set; }
+        
         /// <summary> 
         /// Gets or sets the description
         /// </summary>
         public string Description { get; set; }
+        
         /// <summary>
         /// Gets or sets the resource type
         /// </summary>
         public string ResourceType { get; set; }
+        
         /// <summary>
         /// Gets or sets the parent organization id
         /// </summary>
         public string ParentOragnizationId { get; set; }
+
         /// <summary>
         /// Gets or sets the linked organizations ids
         /// </summary>
